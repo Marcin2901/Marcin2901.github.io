ed60f8d1d1fe4cc93a7e18d6e3b24c2cb2c35ed3
Chudy4822 <jamarcin2901@wp.pl>
2020-11-22 00:17:22 +0100

    Trello 1.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://protected-basin-11096.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://protected-basin-11096.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
